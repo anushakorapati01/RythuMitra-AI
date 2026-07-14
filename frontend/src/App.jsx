64e0527eecb1e13ba2abc4fb85df765e8c7ffac9
anushakorapati01 <anushakorapati01@gmail.com>
2026-07-14 13:32:55 +0530

    Add reusable navigation bar component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="app">
+        <div className="content">
+
+          <h1>🌾 RythuMitra AI</h1>
+
+          <h2>Your Smart Farming Assistant</h2>
+
+          <p>
+            Empowering Farmers with Artificial Intelligence
+          </p>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
